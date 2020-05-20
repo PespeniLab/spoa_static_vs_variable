67a973d3f9b9ce4dabc938a79c8a56d341ff711b
rsbrennan <reid.brennan@gmail.com>
2020-05-20 16:16:57 -0400

    calc p-vals, posthocs.

--- snp_class_plot.R
+++ snp_class_plot.R
@@ -70,22 +70,34 @@ for(n.in in 1:6){
 
 prop.out$total <- as.numeric(prop.out$n_TRUE) + as.numeric(prop.out$n_FALSE)
 prop.out$n_TRUE <- as.integer(prop.out$n_TRUE)
-# now get CIs
 
-prop.out2 <-mutate(prop.out,      
+# now get CIs. Based on binomial sampling. with expected as the probability. 
+  # largely following: https://rcompanion.org/rcompanion/b_03.html
+# this calculates the CI using the Clopper-Pearson method
+# can also use this as a posthoc test to identify which loci are driving the chi-sq diffs we observe.
+
+prop.out2 <-mutate(prop.out,
        low.ci = apply(prop.out[c("n_TRUE", "total", "Expected")],
                        1,
                        function(x)
                        binom.test(x["n_TRUE"], x["total"], x["Expected"]
                                  )$ conf.int[1]),
-                                
+
         upper.ci = apply(prop.out[c("n_TRUE", "total", "Expected")],
                        1,
                        function(x)
                        binom.test(x["n_TRUE"], x["total"], x["Expected"]
-                                 )$ conf.int[2])
+                                 )$ conf.int[2]),
+        p.val = apply(prop.out[c("n_TRUE", "total", "Expected")],
+                       1,
+                       function(x)
+                       binom.test(x["n_TRUE"], x["total"], x["Expected"]
+                                 )$ p.value)        
          )
 
+
+prop.out2$p.val [prop.out2$Value == "expected"] <- NA
+prop.out2$p.val [prop.out2$Value == "expected"] <- NA
 
 prop.out2$low.counts<- prop.out2$low.ci*prop.out2$total
 prop.out2$high.counts<- prop.out2$upper.ci*prop.out2$total
@@ -100,25 +112,6 @@ prop.out2$group[which(prop.out2$group == "DAY7_D7_7_0_V")] <- c("pH 7.0 V")
 prop.out2$group[which(prop.out2$group == "all_groups_combined")] <- c("All groups combined")
 
 prop.out2$snp_class <- factor(prop.out2$snp_class, c("upstream", "exon","intron" ,"intergenic"))
-
-
-
-p.out <- ggplot(prop.out2,
-   aes(x = snp_class, y = as.numeric(n_TRUE), fill = Value))  +
-       geom_bar(aes(y=n_TRUE),stat="identity", width = 0.4, position = "dodge", color="black")  +
-       #geom_errorbar(position=position_dodge(width=0.9), color="black")    +   ## ggtitle("Main title") +
-       theme_bw() + facet_wrap(vars(group), scales = "free") +
-          geom_errorbar(position=position_dodge(width=0.4),
-                     width=0.0, size=0.5, color="black",
-                     aes(ymax=low.counts, ymin=high.counts))+
-          theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-          xlab("") + ylab("number of significant loci") +
-          scale_fill_manual(values = c("darkgrey", "lightgoldenrod2"))+
-            theme(legend.title=element_blank())
-
-
-
-ggsave(p.out, file="~/Documents/UVM/spoa/figures/FigS1_class_sig_pvals.pdf", h=5, w=9)
 
 # print out xsq pvals, skip combined
 for(n.in in 1:6){
@@ -139,5 +132,40 @@ for(n.in in 1:6){
 
 # mult testing correct, skipping the combined cat.
 p.adjust(c(0.26, 0.082, 0.033, 3e-14, 2.1e-05), method="fdr")
+ # 2.6e-01 1.0e-01 5.5e-02 1.5e-13 5.2e-05
+# adj pvalues for each posthoc
 
+for(i in unique(prop.out2$group)){
+  tmpidx <- which(prop.out2$group == i)
+  prop.out2$p.val.adj[tmpidx] <- p.adjust(prop.out2$p.val[tmpidx], method ="fdr")
+}
+
+label.pval <- data.frame(group = unique(prop.out2$group),
+                       chisq.pval = c("P < 0.001", "P = 0.26", "P = 0.1", "P = 0.06", "P < 0.001", "P < 0.001"))
+
+prop.out3 <- left_join(prop.out2, label.pval)
+
+p.out <- ggplot(prop.out3,
+   aes(x = snp_class, y = as.numeric(n_TRUE), fill = Value))  +
+       geom_bar(aes(y=n_TRUE),stat="identity", width = 0.4, position = "dodge", color="black")  +
+       #geom_errorbar(position=position_dodge(width=0.9), color="black")    +   ## ggtitle("Main title") +
+       theme_bw() + facet_wrap(vars(group), scales = "free") +
+          geom_errorbar(position=position_dodge(width=0.4),
+                     width=0.0, size=0.5, color="black",
+                     aes(ymax=low.counts, ymin=high.counts))+
+          theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+          xlab("") + ylab("number of significant loci") +
+          scale_fill_manual(values = c("darkgrey", "lightgoldenrod2"))+
+            theme(legend.title=element_blank())
+
+# add significance values:
+
+p.out1 <- p.out +   geom_point(data = prop.out3[which(prop.out2$p.val.adj < 0.05 & prop.out2$snp_class == "exon"),],
+    aes(x=snp_class, y=high.counts*1.05),shape = "*", size=8, show.legend = FALSE) +
+        geom_text(data = label.pval, aes(x = "intergenic", y = Inf, label = chisq.pval, group = group),
+        size = 3,
+        vjust = 1.8,
+        inherit.aes = FALSE)
+
+ggsave(p.out1, file="~/Documents/UVM/spoa/figures/FigS1_class_sig_pvals.pdf", h=5, w=9)
 
